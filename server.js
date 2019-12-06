d47a13cc09e5c1adaf176cdd5c9a07f15e1b68be
Bradley Mulick <b.radleymulick@icloud.com>
2019-12-06 09:55:29 -0700

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const path = require("path")
 require("dotenv").config()
 
